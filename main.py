c43237f1945de755c3580c81fba1b1ed95f0a95b
sunaydagli <sunaydagli@berkeley.edu>
2021-04-14 00:43:27 -0700

    heroku p2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from re import sub
 
+
 import nltk
 from sys import argv
 from io import open
@@ -70,5 +71,6 @@ def summarize(text):
 # if __name__ == "__main__":
 #     app.run(host="127.0.0.1", port=8080, debug=True)
 
-# if __name__ == "__main__":
-#     app.run(host="127.0.0.1", port=8000, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
